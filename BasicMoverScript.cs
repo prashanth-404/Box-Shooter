faf2c52087fcea962128fe9b4191c07c33b2acc4
prashanth-404 <65747581+prashanth-404@users.noreply.github.com>
2020-05-23 20:42:01 +0530

    Add files via upload

---
+++ BasicMoverScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMoverScript : MonoBehaviour
+{
+    public enum motionDirection { Spin, Horizontal, Vertical };
+    public motionDirection motionstate = motionDirection.Horizontal;
+
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (motionstate)
+        {
+            case motionDirection.Spin:
+                gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+                break;
+            case motionDirection.Horizontal:
+                gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+            case motionDirection.Vertical:
+                gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+        }
+    }
+}
